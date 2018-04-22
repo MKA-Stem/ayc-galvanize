6c90120cb34aee6e7e0d15f92a50838078b94c05
Mark Apinis <mapinis2019@gmail.com>
2018-04-22 11:34:34 -0400

    First two endpoints

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import compression from 'compression';
-import {resolve} from 'path';
+import { resolve } from 'path';
 import fs from 'fs';
 import db from './data/db';
 
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 8080;
 const DEV = process.env.NODE_ENV === 'development';
 
 const app = express();
+
+// Parse request bodies
+app.use(bodyParser.json());
+
+// Good Logging
+app.use(require('morgan')('dev'));
 
 // Compress all requests.
 app.use(compression());
@@ -37,36 +43,64 @@ if (!DEV && indexPath) {
 // Serve SPA files
 app.use(express.static(SPA_ROOT));
 
-app.get('*', (req, res, next) => {
-  res.sendFile(SPA_ROOT + '/index.html');
-});
-
 // API Endpoints
-app.post('/api/newMessage', (req, res) => {
+app.post('/api/newMessage', async (req, res) => {
   /*
     {
-      phoneNum: "String",
+      phone: "String",
       key: "String"
     }
 
     Hashes Key and puts all in keys table
   */
+
+  if (req.body.phone && req.body.key) {
+
+    const hash = req.body.key; // hashing to come later
+
+    await db('keys').insert({
+      hash: hash,
+      phone: req.body.phone,
+      key: req.body.key
+    });
+
+    res.status(200).send({ status: "ok" });
+
+  } else {
+    res.status(400).send({ error: "missing required parameters" });
+  }
 });
 
-app.post('/api/getPhoneNum', (req, res) => {
+app.post('/api/getPhone', async (req, res) => {
   /*
     {
-      keyHash: "String"
+      hash: "String"
     }
 
-    Returns phoneNum based on keyHash from keys table
+    Returns phone based on keyHash from keys table
   */
+
+  if (req.body.hash) {
+
+    const phone = await db('keys').where({
+      hash: req.body.hash
+    }).select('phone');
+
+    if(phone[0]) {
+      res.status(200).send(phone[0]);
+    } else {
+      res.status(404).send({ error: "hash not found"})
+    }
+
+  } else {
+    res.status(400).send(error: "missing required paramaters");
+  }
 });
 
 app.post('/api/send2FA', (req, res) => {
   /*
     {
-      keyHash: "String"
+      hash: "String"
     }
 
     Sends 2FA to phone number based on keyHash in keys table
@@ -82,6 +116,10 @@ app.post('/api/verify2FA', (req, res) => {
 
     Verifies the code then sends unhashed key if good
   */
+});
+
+app.get('*', (req, res, next) => {
+  res.sendFile(SPA_ROOT + '/index.html');
 });
 
 app.listen(PORT, () => {
