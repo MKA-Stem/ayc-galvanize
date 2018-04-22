37a07daebd82bfadd42f1aca20987cfc83a5ac74
AaronCGoidel <agoidel2019@mka.org>
2018-04-22 13:37:29 -0400

    crypto functions

---
+++ client/src/lib/cryptography.js
@@ -0,0 +1,22 @@
+var CryptoJS = require("crypto-js");
+
+export function encrypt(message, key) {
+  var ciphered = CryptoJS.AES.encrypt(message, key);
+  console.log(ciphered);
+  return(ciphered);
+}
+
+export function decrypt(cyphered, key){
+  var bytes  = CryptoJS.AES.decrypt(cyphered, key);
+  var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  console.log(decryptedData);
+  return decryptedData
+}
+
+export function genKey() {
+  var ret = "";
+  while (ret.length < 58) {
+    ret += Math.random().toString(16).substring(2);
+  }
+  return ret.substring(0,58);
+}
